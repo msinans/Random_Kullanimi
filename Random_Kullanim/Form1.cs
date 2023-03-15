a8d8195c5ae1ce38babc38253168cf90548ec239
m.sinans <msomercikoglu@gmail.com>
2023-03-15 13:43:12 +0300

    Add project files.

---
+++ Random_Kullanim/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Random_Kullanim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int s1, s2, s3;
+            s1 = rnd.Next(1, 5);
+            s2 = rnd.Next(1, 5);
+            s3 = rnd.Next(1, 5);
+            label1.Text = s1.ToString();
+            label2.Text = s2.ToString();
+            label3.Text = s3.ToString();
+
+            if (s1 == 3 && s2 == 3 && s3 ==3)
+            {
+                MessageBox.Show("Tebrikler Kazandınız!");
+            }
+        }
+    }
+}
